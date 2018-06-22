b488942a77810a81bbc1737c8576dd149e1b82d5
Tim de Reuse <timdereuse@gmail.com>
2018-06-22 15:37:23 -0400

    rectangles split and cropped

--- textAlign.py
+++ textAlign.py
@@ -3,59 +3,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 gc.init_gamera()
 
-input_image = gc.load_image('./CF-010_3.png')
-
-#first order of business: fix rotation
-
 #cut_tolerance_x should be larger than the maximum space between words on the
 #same line
 #cut_tolerance_y should be smaller than the vertical space between lines of text
+input_image = gc.load_image('./CF-010_3.png')
 cut_tolerance_x = 100
 cut_tolerance_y = 100
 cut_tolerance_noise = 300
 prune_small_cuts_tolerance = 5
+below_cuts_tolerance = 0.5
 
 onebit = input_image.to_onebit()
 angle,tmp = onebit.rotation_angle_projections()
-onebit = onebit.rotate(angle = angle, order = 2)
+onebit = onebit.rotate(angle = angle, order = 3)
 
 cuts = onebit.projection_cutting(cut_tolerance_x,cut_tolerance_y,cut_tolerance_noise,1)
 
 #reject sufficiently small cuts likely to have captured noise;
-#reject all cuts whose vertical height is one standard deviation smaller than
-#the mean heignt
+#reject all cuts whose vertical height is sufficiently small.
+#size threshold: median of all absolute distances from median.
 
 med_height = np.median([x.nrows for x in cuts])
 std_height = np.median([abs(x.nrows - med_height) for x in cuts])
 
 cuts[:] = [x for x in cuts if x.nrows > med_height - (std_height * prune_small_cuts_tolerance)]
 
-#find minimum projection value x such that all points in middle are above
-#x and all points to either side are below.
+#the cuts are rather harsh
+rectangles = []
+for n in range(1,len(cuts)):
 
-# def projection_crossing_pts(proj,test_val):
-#
-#     crossing_points = []
-#     for n in range(1,len(proj)):
-#
-#         cross_up = proj[n] - test_val
-#         cross_down = proj[n-1] - test_val
-#
-#         if not np.sign(cross_up) == np.sign(cross_down):
-#             crossing_points.append(n)
-#
-#     return crossing_points
-#
-# val_increment = 5
-# test_val = 5
-# crossing_point_groups = []
-# val_found = False
-#
-# while (not val_found):
+    add_to_bottom = int(cuts[n].nrows * below_cuts_tolerance)
 
+    point_ul = gc.Point(
+        cuts[n].offset_x,
+        cuts[n-1].offset_y + cuts[n-1].nrows
+        )
+    point_lr = gc.Point(
+        cuts[n].offset_x + cuts[n].ncols,
+        cuts[n].offset_y + cuts[n].nrows + add_to_bottom
+        )
 
+    rectangles.append( gc.SubImage(onebit,point_ul,point_lr))
 
-#onebit.save_image("testimg.png")
+#go back and insert first row
+add_to_bottom = int(cuts[0].nrows * below_cuts_tolerance)
+
+point_ul = gc.Point(
+    cuts[0].offset_x,
+    cuts[0].offset_y - rectangles[0].nrows
+    )
+point_lr = gc.Point(
+    cuts[0].offset_x + cuts[0].ncols,
+    cuts[0].offset_y + cuts[0].nrows + add_to_bottom
+    )
+
+rectangles.insert(0,gc.SubImage(onebit,point_ul,point_lr))
 
 def imsv(img):
     img.save_image("testimg.png")
