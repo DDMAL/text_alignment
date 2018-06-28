be40cf7f85563d4c52b8076478b82b3835fc82bb
Tim de Reuse <timdereuse@gmail.com>
2018-06-28 12:32:59 -0400

    _group_ccs method added to group connected components

--- textAlign.py
+++ textAlign.py
@@ -5,17 +5,23 @@ from gamera.plugins.image_utilities import union_images
 import numpy as np
 gc.init_gamera()
 
+
+input_image = gc.load_image('./CF-011_3.png')
+
+#parameters to use for inital projection cut, using gamera's built-in projection cutting method
 #cut_tolerance_x should be large enough to totally discourage vertical slices
 #cut_tolerance_y should be smaller than the vertical space between lines of text
 #cut_tolerance_noise allows some amount of noise to be seen as a `gap` for cutting thru
-input_image = gc.load_image('./CF-011_3.png')
 cut_tolerance_x = 1000
 cut_tolerance_y = 10
 cut_tolerance_noise = 300
-noise_area_thresh = 200         #ignore connected components with area smaller than this
-prune_small_cuts_tolerance = 2
 
-def bases_coincide(slice_offset,slice_nrows,comp_offset,comp_nrows):
+noise_area_thresh = 500         #an int > 0: ignore ccs with area smaller than this
+prune_small_cuts_tolerance = 2  #in [1, inf]: get rid of cuts with size this many stdvs below mean
+base_collision_size = 0.25      #in [0,1]; amt of each cc to consider when clipping
+horizontal_gap_tolerance = 10   #value in pixels
+
+def _bases_coincide(slice_offset, slice_nrows, comp_offset, comp_nrows, base_collision = base_collision_size):
     """
     A helper function that takes in the vertical width of a horizontal strip
     and the vertical measurements of a connected component, and returns a value
@@ -26,13 +32,54 @@ def bases_coincide(slice_offset,slice_nrows,comp_offset,comp_nrows):
     """
 
     component_base = comp_offset + comp_nrows
-    component_height = min(slice_nrows,comp_nrows)
+    #component_height = min(slice_nrows,comp_nrows)
+    component_height = int(np.ceil(comp_nrows * base_collision_size))
 
     range1 = range(slice_offset, slice_offset + slice_nrows)
     range2 = range(component_base - component_height, component_base)
     check = set(range1).intersection(range2)
 
     return bool(check)
+
+def _group_ccs(cc_list, gap_tolerance = horizontal_gap_tolerance):
+    '''
+    a helper function that takes in a list of ccs on the same line and groups them together based
+    on contiguity of their bounding boxes along the horizontal axis.
+    '''
+
+    cc_copy = cc_list[:]
+    result = [[cc_copy.pop(0)]]
+
+    while(cc_copy):
+
+        current_group = result[-1]
+        left_bound = min([x.offset_x for x in current_group]) - gap_tolerance
+        right_bound = max([x.offset_x + x.ncols for x in current_group]) + gap_tolerance
+
+        overlaps = [x for x in cc_copy if
+            (left_bound <= x.offset_x <= right_bound) or
+            (left_bound <= x.offset_x + x.ncols <= right_bound)
+            ]
+
+        if not overlaps:
+            result.append([cc_copy.pop(0)])
+            print('skipping to new list')
+            continue
+
+        print('adding {}'.format(len(overlaps)))
+
+        for x in overlaps:
+            result[-1].append(x)
+            cc_copy.remove(x)
+
+    gap_sizes = []
+    for n in range(len(result)-1):
+        left = result[n][0].offset_x
+        right = result[n][-1].offset_x + result[n][-1].ncols
+        gap_sizes.append(right - left)
+
+    return result, gap_sizes
+
 
 #find likely rotation angle and correct
 one_bit = input_image.to_onebit()
@@ -58,10 +105,11 @@ cc_lines = []
 for cut in cuts:
 
     res = [x for x in components if
-        bases_coincide(cut.offset_y,cut.nrows,x.offset_y,x.nrows)]
+        _bases_coincide(cut.offset_y,cut.nrows,x.offset_y,x.nrows)]
 
     res = sorted(res,key=lambda x: x.offset_x)
     cc_lines.append(res)
+
 
 
 
