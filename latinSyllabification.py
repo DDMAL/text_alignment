29eea9d9c80f783ba76662523be74cc59c7d9bca
timothydereuse <timdereuse@gmail.com>
2019-06-11 15:40:34 -0400

    syllabification completely reworked, much more robust

--- latinSyllabification.py
+++ latinSyllabification.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-
-
 import re
 
-consonant_groups = ['qu', 'fl', 'fr', 'st', 'br', 'cr', 'pr', 'tr', 'ct', 'th']
-dipthongs = ['au', 'io', 'ihe', 'oe', 'ua', 'ui', 'uo']
+consonant_groups = ['qu', 'ch', 'ph', 'fl', 'fr', 'st', 'br', 'cr', 'pr', 'tr', 'ct', 'th']
+diphthongs = ['ae', 'au', 'io', 'ihe', 'oe', 'ua', 'iu']
 vowels = ['a', 'e', 'i', 'o', 'u']
 
 abbreviations = {
@@ -20,21 +18,105 @@ abbreviations = {
 }
 
 
-def syllabify_word(word):
+def syllabify_word(inp):
+    '''
+    separate each word into UNITS - first isolate consonant groups, then diphthongs, then letters.
+    each vowel / diphthong unit is a "seed" of a syllable; consonants and consonant groups "stick"
+    to adjacent seeds. first make every vowel stick to its preceding consonant group. any remaining
+    consonant groups stick to the vowel behind them.
+    '''
+
+    if inp == 'euouae':
+        return 'e-u-o-u-ae'.split('-')
+
+    word = [inp]
+
+    for unit in consonant_groups + diphthongs:
+        new_word = []
+        for segment in word:
+            if '*' in segment:
+                new_word.append(segment)
+                continue
+
+            split = segment.split(unit)
+
+            rep_list = [unit + '*'] * len(split)
+            interleaved = [val for pair in zip(split, rep_list) for val in pair]
+
+            # remove blanks and chop off last extra entry caused by list comprehension
+            interleaved = [x for x in interleaved[:-1] if len(x) > 0]
+            new_word += interleaved
+        word = list(new_word)
+
+    # now split into individual characters anything remaining
+    new_word = []
+    for segment in word:
+        if '*' in segment:
+            new_word.append(segment.replace('*', ''))
+            continue
+        new_word += list(segment)
+    word = list(new_word)
+
+    # add marker to units to mark vowels or diphthongs this time
+    for i in range(len(word)):
+        if word[i] in vowels + diphthongs:
+            word[i] = word[i] + '*'
+
+    # begin merging units together.
+    # first stick consonants / consonant groups to syllables ahead of them
+    new_word = []
+    i = 0
+    while i < len(word):
+        if i + 1 >= len(word):
+            new_word.append(word[i])
+            break
+        cur = word[i]
+        proc = word[i + 1]
+        if '*' in proc and '*' not in cur:
+            new_word.append(cur + proc)
+            i += 2
+        else:
+            new_word.append(cur)
+            i += 1
+
+    word = list(new_word)
+
+    # then stick consonants / consonant groups to syllables behind them
+    new_word = []
+    i = 0
+    while i < len(word):
+        if i + 1 >= len(word):
+            new_word.append(word[i])
+            break
+        cur = word[i]
+        proc = word[i + 1]
+        if '*' in cur and '*' not in proc:
+            new_word.append(cur + proc)
+            i += 2
+        else:
+            new_word.append(cur)
+            i += 1
+
+    word = [x.replace('*', '') for x in new_word]
+
+    return word
+
+
+def syllabify_word_old(word):
     res = word
 
     if res == 'euouae':
         return 'e-u-o-u-ae'
 
-    # put square brackets around all dipthongs and vowels (using lookbehind/lookahead regex to make sure that vowels are not found within dipthongs)
-    for dt in dipthongs:
+    # put square brackets around all diphthongs and vowels (using lookbehind/lookahead regex to make sure that vowels are not found within diphthongs)
+    for dt in diphthongs:
         res = res.replace(dt, '[' + dt + ']')
 
     for v in vowels:
         regex = r"(?<!\[)" + v + r"(?!\])"
         res = re.sub(regex, '[' + v + ']', res)
 
-    vowel_positions = [m.start() for m in re.finditer('\]', res)]
+    vowel_positions = [m.start() for m in re.finditer(r'\]', res)]
     dashes_added = 0
 
     for vp in vowel_positions:
@@ -81,9 +163,7 @@ def syllabify_word(word):
 def syllabify_text(input):
     words = input.split(' ')
     word_syls = [syllabify_word(w) for w in words]
-    syls = []
-    for ws in word_syls:
-        syls += ws.split('-')
+    syls = [item for sublist in word_syls for item in sublist]
     return syls
 
 
@@ -126,4 +206,7 @@ def parse_transcript(filename, syllabify=True):
 
 
 if __name__ == "__main__":
-    print(parse_transcript('./png/salzinnes_18_transcript.txt'))
+    # print(parse_transcript('./salzinnes_ocr/salzinnes_018_ocr.txt'))
+    inp = 'quaecumque eius michi antiphonum dominus'
+    res = syllabify_text(inp)
+    print(res)
