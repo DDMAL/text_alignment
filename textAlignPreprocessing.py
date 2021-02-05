0c39c0a5ecc26937da1bacfd48d028c160cb7141
timothydereuse <timdereuse@gmail.com>
2021-02-05 15:52:44 -0500

    cleaned unnecessary methods from preproc

--- textAlignPreprocessing.py
+++ textAlignPreprocessing.py
@@ -1,19 +1,18 @@
 from os.path import isfile, join
 import numpy as np
-# import gamera.core as gc
-# gc.init_gamera()
 import matplotlib.pyplot as plt
-# from gamera.plugins.image_utilities import union_images
 import pickle
 import itertools as iter
 import os
 import re
+import cv2 as cv
+
 
 # PARAMETERS FOR PREPROCESSING
 saturation_thresh = 0.9
 sat_area_thresh = 150
-despeckle_amt = 100            # an int in [1,100]: ignore ccs with area smaller than this
-noise_area_thresh = 100        # an int in : ignore ccs with area smaller than this
+soften_amt = 5          # size of gaussian blur to apply before taking threshold
+fill_holes = 3          # size of kernel used for morphological operations when despeckling
 
 # PARAMETERS FOR TEXT LINE SEGMENTATION
 filter_size = 30                # size of moving-average filter used to smooth projection
@@ -24,27 +23,6 @@ remove_capitals_scale = 10000   # removes large ccs. turned off for now
 # CC GROUPING (BLOBS)
 cc_group_gap_min = 20  # any gap at least this wide will be assumed to be a space between words!
 max_distance_to_staff = 200
-
-
-def vertically_coincide(hline_position, comp_offset, comp_nrows, collision, collision_scale=collision_strip_scale):
-    """
-    A helper function that takes in the vertical width of a horizontal strip
-    and the vertical measurements of a connected component, and returns a value
-    of True if any part of it lies within the strip.
-    """
-
-    collision *= collision_strip_scale
-
-    component_top = comp_offset
-    component_bottom = comp_offset + comp_nrows
-
-    strip_top = hline_position - int(collision / 2)
-    strip_bottom = hline_position + int(collision / 2)
-
-    both_above = component_top < strip_top and component_bottom < strip_top
-    both_below = component_top > strip_bottom and component_bottom > strip_bottom
-
-    return (not both_above and not both_below)
 
 
 def calculate_peak_prominence(data, index):
@@ -148,130 +126,28 @@ def moving_avg_filter(data, filter_size=filter_size):
     return smoothed
 
 
-def preprocess_images(input_image, despeckle_amt=despeckle_amt, filter_runs=1, filter_runs_amt=2, correct_rotation=True):
+def preprocess_images(input_image, soften=soften_amt, fill_holes=fill_holes, correct_rotation=True):
     '''
     use gamera to do some denoising, etc on the text layer before attempting text line
     segmentation
     '''
 
-    image_bin = input_image.to_onebit()
+    print(f'processing {fname}...')
 
-    image_bin.despeckle(despeckle_amt)
-    image_bin.invert()
-    image_bin.despeckle(despeckle_amt)
-    image_bin.invert()
+    gray_img = cv.cvtColor(input_image, cv.COLOR_BGR2GRAY)
 
-    # keep only colored ccs above a certain size
-    ccs = image_bin.cc_analysis()
-    for c in ccs:
-        area = c.nrows
-        if sat_area_thresh < area:
-            c.fill_white()
+    blur = cv.GaussianBlur(gray_img, (5, 5), 0)
+    ret3, img_bin = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
-    # image_bin = input_image.to_onebit().subtract_images(image_bin)
+    kernel = np.ones((fill_holes, fill_holes), np.uint8)
+    cl = cv.morphologyEx(img_bin, cv.MORPH_CLOSE, kernel)
+    img_eroded = cv.morphologyEx(cl, cv.MORPH_OPEN, kernel)
 
-    # find likely rotation angle and correct
-    angle, tmp = image_bin.rotation_angle_projections(-6, 6)
-    if correct_rotation:
-        image_bin = image_bin.rotate(angle=angle)
+    line_strips, lines_peak_locs, proj = identify_text_lines(img_eroded)
 
-    image_bin.reset_onebit_image()
+    angle = 0
 
-    image_eroded = image_bin.image_copy()
-
-    for i in range(filter_runs):
-        image_eroded.filter_short_runs(filter_runs_amt, 'black')
-        image_eroded.filter_narrow_runs(filter_runs_amt, 'black')
-
-    return image_bin, image_eroded, angle
-
-
-def identify_text_lines_old(image_bin, image_eroded):
-    '''
-    finds text lines on preprocessed image. step-by-step:
-    1. find peak locations of vertical projection
-    2. draw horizontal white lines between peak locations to make totally sure that lines are
-        unconnected (ornamental letters can often touch the line above them)
-    3. connected component analysis
-    4. break into neat rows of connected components that each intersect the same horizontal line
-    5. deal with some pathological cases (empty lines, doubled lines, etc)
-    '''
-
-    # compute y-axis projection of input image and filter with sliding window average
-    project = image_eroded.projection_rows()
-    smoothed_projection = moving_avg_filter(project, filter_size)
-
-    # calculate normalized log prominence of all peaks in projection
-    peak_locations = find_peak_locations(smoothed_projection)
-
-    # draw a horizontal white line at the local minima of the vertical projection. this ensures
-    # that every connected component can intersect at most one text line.
-    for i in range(len(peak_locations) - 1):
-        start = peak_locations[i]
-        end = peak_locations[i + 1]
-        idx = np.argmin(smoothed_projection[start:end])
-        idx += start
-        image_eroded.draw_line((0, idx), (image_eroded.ncols, idx), 0, 2)
-
-    # perform connected component analysis and remove sufficiently small ccs and ccs that are too
-    # tall; assume these to be ornamental letters
-    components = image_eroded.cc_analysis()
-
-    for c in components:
-        if c.black_area()[0] < noise_area_thresh:
-            c.fill_white()
-
-    components[:] = [c for c in components if c.black_area()[0] > noise_area_thresh]
-
-    med_comp_height = np.median([c.nrows for c in components])
-
-    components[:] = [c for c in components if c.nrows < (med_comp_height * remove_capitals_scale)]
-
-    # using the peak locations found earlier, find all connected components that are intersected by
-    # a horizontal strip at either edge of each line. these are the lines of text in the manuscript
-
-    line_strips = []
-
-    cc_median_height = np.median([x.nrows for x in components])
-    cc_lines = []
-    for line_loc in peak_locations:
-        res = [x for x in components if vertically_coincide(line_loc, x.offset_y, x.nrows, cc_median_height)]
-
-        ulx = min(s.ul.x for s in res)
-        uly = min(s.ul.y for s in res)
-        lrx = max(s.lr.x for s in res)
-        lry = max(s.lr.y for s in res)
-
-        strip = image_bin.subimage((ulx, uly), (lrx, lry))
-        line_strips.append(strip)
-
-    # if a single connected component appears in more than one cc_line, give priority to the line
-    # that is closer to the center of the component's bounding box
-    # for n in range(len(cc_lines) - 1):
-    #     intersect = set(cc_lines[n]) & set(cc_lines[n + 1])
-    #
-    #     # if most of the ccs are shared between these lines, just delete one of them
-    #     if len(intersect) > (0.5 * min(len(cc_lines[n]), len(cc_lines[n + 1]))):
-    #         cc_lines[n] = []
-    #         continue
-    #
-    #     for i in intersect:
-    #
-    #         box_center = i.offset_y + (i.nrows / 2)
-    #         distance_up = abs(peak_locations[n] - box_center)
-    #         distance_down = abs(peak_locations[n + 1] - box_center)
-    #
-    #         if distance_up > distance_down:
-    #             cc_lines[n].remove(i)
-    #             # print('removing up')
-    #         else:
-    #             cc_lines[n+1].remove(i)
-    #             # print('removing down')
-
-    # remove all empty lines from cc_lines in case they've been created by previous steps
-    # cc_lines[:] = [x for x in cc_lines if bool(x)]
-
-    return line_strips, peak_locations, smoothed_projection
+    return img_bin, img_eroded, angle
 
 
 def identify_text_lines(image_eroded):
@@ -355,8 +231,6 @@ def rotate_image(image, angle):
     return result
 
 
-
-
 if __name__ == '__main__':
     from PIL import Image, ImageDraw, ImageFont
     from matplotlib import pyplot as plt
@@ -369,23 +243,11 @@ if __name__ == '__main__':
         print(f'processing {fname}...')
         raw_image = cv.imread(f'./png/{fname}_text.png')
 
-        gray_img = cv.cvtColor(raw_image, cv.COLOR_BGR2GRAY)
+        img_bin, img_eroded, angle = preprocess_images(raw_image, soften=soften_amt, fill_holes=3)
 
-        blur = cv.GaussianBlur(gray_img, (5, 5), 0)
-        ret3, th3 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+        # cv.imwrite('test.png', img_eroded)
 
-        kernel = np.ones((3, 3), np.uint8)
-        cl = cv.morphologyEx(th3, cv.MORPH_CLOSE, kernel)
-        eroded = cv.morphologyEx(cl, cv.MORPH_OPEN, kernel)
-
-        # cv.imwrite('test.png', eroded)
-
-        line_strips, lines_peak_locs, proj = identify_text_lines(eroded)
-
-
-
-        #
-
+        line_strips, lines_peak_locs, proj = identify_text_lines(img_eroded)
         # save_preproc_image(image, line_strips, lines_peak_locs, fname)
 
     # plt.clf()
